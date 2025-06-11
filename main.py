c5cfc7701c653c8c74c740623218b14f83e910e5
NeLyu <lyu.klimenchenko@gmail.com>
2025-06-11 13:18:30 +0200

    Fixed the issue with showing less than five channels

--- main.py
+++ main.py
@@ -19,21 +19,29 @@ def main():
     extended_query = ac.call_openai(prompt_w_query)
 
     print("Extracting channels...")
-    n = 30
+    n = 10
     sorted_channels = ac.extract_channels(extended_query, youtube, n)
 
     print("Selecting only relevant channels...")
-    channels_selected = ac.select_relevant_channels(sorted_channels, query, 0, 25)
-    print()
+    channels_selected = ac.select_relevant_channels(sorted_channels, query, 0, round(len(sorted_channels)/2))
 
     print("Summarizing...")
     pt = load_prompt_template("summarize")
     prompt_for_summary = pt.format(query=query, channels_selected=channels_selected)
     summary = ac.call_openai(prompt_for_summary)
+
+    top_to_show = 5
+    if len(channels_selected) < top_to_show:
+        top_to_show = len(channels_selected)
+    
     print("Recommended channels:")
-    for i in range(len(channels_selected)):
+    for i in range(top_to_show):
         print(f"""{i + 1}. {channels_selected[i]["title"]}""")
         print(channels_selected[i]["url"])
+        print(f"Treding video from the channel:")
+        print(channels_selected[i]["video_title"])
+        print(channels_selected[i]["videoId"])
+        print()
 
     print()
     print(summary)
